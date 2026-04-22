3680b276941936c878862250e34584a7974b4b84
efarfan-dev <efarfan.dev@gmail.com>
2026-04-22 01:37:29 -0400

    Implement deck creation and basic Blackjack UI

--- src/App.java
+++ src/App.java
@@ -1,2 +1,7 @@
-public class App {
+public class App{
+    public static void main(String[]args){
+        BlackJack blackJack = new BlackJack();
+
+    }
 }
+
---
+++ src/BlackJack.java
@@ -0,0 +1,156 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+        Card(String value, String type) {
+            this.value = value;
+            this.type = type;
+        }
+
+        public String toString(){
+            return value + "-" + type;
+        }
+
+        public int getValue(){
+            if("AJQK".contains(value)){ // A J K Q
+                if(value == "A") {
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value); //2-10
+        }
+
+        public boolean isAce(){
+            return value == "A";
+        }
+
+
+}
+
+ArrayList<Card> deck;
+    Random random = new Random(); //shuffle deck
+
+    //dealer
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;
+
+    //user
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
+
+    //window
+    int boardWidth = 600;
+    int boardHeight = boardWidth;
+
+    JFrame frame = new JFrame("Black Jack");
+    JPanel gamePanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JButton hitButton = new JButton("Hit");
+    JButton stayButton = new JButton("Stay");
+
+
+    BlackJack(){
+        startGame();
+
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gamePanel.setLayout(new BorderLayout());
+        gamePanel.setBackground(new Color(53, 101, 77));
+        frame.add(gamePanel);
+
+        hitButton.setFocusable(false);
+        buttonPanel.add(hitButton);
+        stayButton.setFocusable(false);
+        buttonPanel.add(stayButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+    }
+
+    public void startGame() {
+        //deck
+        buildDeck();
+        shuffleDeck();
+
+        //dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.remove(deck.size()-1);
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1:0;
+
+        Card card = deck.remove(deck.size()-1);
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+        System.out.println("Dealer: ");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+
+        //user
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for(int i = 0; i < 2; i++) {
+            card = deck.remove(deck.size()-1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
+
+        System.out.println("Player: ");
+        System.out.println(playerHand);
+        System.out.println(playerSum);
+        System.out.println(playerAceCount);
+
+
+    }
+
+    public void buildDeck(){
+        deck = new ArrayList<Card>();
+        String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String[] types = {"C","D","H","S"};
+
+        for(int i = 0; i < types.length; i++){
+            for(int j = 0; j < values.length; j++ ){
+                Card card = new Card(values[j], types[i]);
+                deck.add(card);
+            }
+        }
+
+        System.out.println("Build Deck:");
+        System.out.println(deck);
+    }
+
+    public void shuffleDeck(){
+        for (int i = 0; i < deck.size(); i++){
+            int j = random.nextInt(deck.size());
+            Card currentCard = deck.get(i);
+            Card randomCard = deck.get(j);
+            deck.set(i, randomCard);
+            deck.set(j, currentCard);
+        }
+
+        System.out.println("After Shuffle");
+        System.out.println(deck);
+    }
+}
